892b4386bf59c5d3d572976a0d23075b258f13f6
hero-gg <105825347+hero-gg@users.noreply.github.com>
2024-04-28 17:34:58 +1200

    undo figure out later

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Wishlist from '@/utils/Wishlist';
 
@@ -14,8 +14,10 @@ interface CardProps extends MediaShort {
 export default function Card({ id, poster, title, type, Ref }: CardProps) {
   const ref = useRef<HTMLAnchorElement>(null);
 
+  const nav = useNavigate();
+
   const [active, setActive] = useState(false);
-  const [setWished] = useState(false);
+  const [wished, setWished] = useState(false);
 
   const [episode, setEpisode] = useState(1);
   const [season, setSeason] = useState(1);
@@ -61,6 +63,40 @@ export default function Card({ id, poster, title, type, Ref }: CardProps) {
     setActive(false);
   }
 
+  function onPlusClick(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    Wishlist.add({ id, poster, title, type });
+  }
+
+  function onCheckClick(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    Wishlist.remove(id, type);
+  }
+
+  function onChevronClick(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    nav(`/${type}/${id}`);
+  }
+
+  useEffect(() => {
+    getContinue();
+
+    setWished(Wishlist.has(id, type));
+
+    function onWishlistChange() {
+      setWished(Wishlist.has(id, type));
+    }
+
+    Wishlist.on(id, type, onWishlistChange);
+
+    return () => {
+      Wishlist.off(id, type, onWishlistChange);
+    };
+  }, []);
+
   useEffect(() => {
     window.addEventListener('click', onWindowClick);
 
@@ -79,6 +115,26 @@ export default function Card({ id, poster, title, type, Ref }: CardProps) {
       onMouseLeave={onCardLeave}
     >
       <img src={poster} alt={title} loading="lazy" />
+
+      <div className="media-card-actions">
+        <button className="button">
+          <i className="fa-solid fa-play"></i>
+        </button>
+
+        {wished ? (
+          <button className="button" onClick={onCheckClick}>
+            <i className="fa-solid fa-check"></i>
+          </button>
+        ) : (
+          <button className="button secondary" onClick={onPlusClick}>
+            <i className="fa-solid fa-plus"></i>
+          </button>
+        )}
+
+        <button className="button secondary right" onClick={onChevronClick}>
+          <i className="fa-solid fa-chevron-down"></i>
+        </button>
+      </div>
     </Link>
   );
 }
