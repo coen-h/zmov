0a45489ccf28ead9e41d0f03061d8b6c1db9461b
hero-gg <105825347+hero-gg@users.noreply.github.com>
2024-04-28 17:24:49 +1200

    might messed up more

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -63,24 +63,6 @@ export default function Card({ id, poster, title, type, Ref }: CardProps) {
     setActive(false);
   }
 
-  function onPlusClick(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-
-    Wishlist.add({ id, poster, title, type });
-  }
-
-  function onCheckClick(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-
-    Wishlist.remove(id, type);
-  }
-
-  function onChevronClick(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-
-    nav(`/${type}/${id}`);
-  }
-
   useEffect(() => {
     getContinue();
 
@@ -115,26 +97,6 @@ export default function Card({ id, poster, title, type, Ref }: CardProps) {
       onMouseLeave={onCardLeave}
     >
       <img src={poster} alt={title} loading="lazy" />
-
-      <div className="media-card-actions">
-        <button className="button">
-          <i className="fa-solid fa-play"></i>
-        </button>
-
-        {wished ? (
-          <button className="button" onClick={onCheckClick}>
-            <i className="fa-solid fa-check"></i>
-          </button>
-        ) : (
-          <button className="button secondary" onClick={onPlusClick}>
-            <i className="fa-solid fa-plus"></i>
-          </button>
-        )}
-
-        <button className="button secondary right" onClick={onChevronClick}>
-          <i className="fa-solid fa-chevron-down"></i>
-        </button>
-      </div>
     </Link>
   );
 }
