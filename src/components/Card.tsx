9051ae4cf25504eacf6cd10149e40558ea3c3f7f
Hero <105825347+hero-gg@users.noreply.github.com>
2024-05-02 11:39:47 +1200

    test again

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -119,7 +119,7 @@ export default function Card({ id, poster, title, type, Ref }: CardProps) {
       <div className="media-card-actions">
         <Link className="button" to={`/watch/${id}${type === 'series' ? `?s=${season}&e=${episode}` : ''}`}>
           <i className="fa-solid fa-play"></i>
-          <span>{type === 'series' ? `S${season} E${episode}` : 'Play'}</span>
+          <span>{type === 'series' ? `S${season} E${episode}` : ''}</span>
         </Link>
 
         {wished ? (
