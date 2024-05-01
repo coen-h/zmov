f2d795a2885751c741510e83522771ca6cbc21ff
Hero <105825347+hero-gg@users.noreply.github.com>
2024-05-02 11:57:25 +1200

    guess 3

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -118,7 +118,7 @@ export default function Card({ id, poster, title, type, Ref }: CardProps) {
 
       <div className="media-card-actions">
         <Link className="button" to={`/watch/${id}${type === 'series' ? `?s=${season}&e=${episode}` : ''}`}>
-          <i className="fa-solid fa-play" style={{ marginLeft: '10px' }}></i>
+          <i className="fa-solid fa-play" style={{ marginLeft: '12px' }}></i>
           <span>{type === 'series' && `S${season} E${episode}`}</span>
         </Link>
 
