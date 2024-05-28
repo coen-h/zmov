4eeb533adac226d302752579425e211a0b6c1a02
coen-h <105825347+coen-h@users.noreply.github.com>
2024-05-28 19:58:31 +1200

    shit, reset api keys

--- scripts/account.js
+++ scripts/account.js
@@ -2,33 +2,30 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
         import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
         const firebaseConfig = {
-            apiKey: "AIzaSyAEXZjHoivvvO_I-mzXKEHBGWqUbWSNu6A",
-            authDomain: "zmov-0.firebaseapp.com",
-            projectId: "zmov-0",
-            storageBucket: "zmov-0.appspot.com",
-            messagingSenderId: "326117586417",
-            appId: "1:326117586417:web:b5b902a295e80793810050",
-            measurementId: "G-K8YZBHVLJ9"
+            apiKey: process.env.FIREBASE_API_KEY,
+            authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.FIREBASE_APP_ID,
+            measurementId: process.env.FIREBASE_MEASUREMENT_ID
         };
 
-        // Initialize Firebase
         const app = initializeApp(firebaseConfig);
         const auth = getAuth(app);
 
-        // Check auth state
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 document.getElementById('account-email').textContent = `Signed in as: ${user.email}`;
                 document.getElementById('account-section').style.display = 'block';
             } else {
-                window.location.href = 'signin.html'; // Redirect to sign-in page if not authenticated
+                window.location.href = 'signin.html';
             }
         });
 
-        // Logout function
         window.logout = function logout() {
             signOut(auth).then(() => {
-                window.location.href = 'signin.html'; // Redirect to sign-in page after sign-out
+                window.location.href = 'signin.html';
             }).catch((error) => {
                 console.error('Error logging out:', error.message);
             });
