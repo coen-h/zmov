896991dd275d5e603b2c0c1d6e4917057cc3c364
coen-h <105825347+coen-h@users.noreply.github.com>
2024-06-14 17:10:54 +1200

    small fixes

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
