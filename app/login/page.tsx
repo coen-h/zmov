8bd2f8c18b2cfa50fd70f6a9331f60e0c0b9a23e
coen-h <105825347+coen-h@users.noreply.github.com>
2025-09-04 22:31:11 +1200

    attempt 1 fix origin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,6 +5,7 @@ export default function LoginPage() {
   const handleGoogleLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
+      options: {    redirectTo: `${window.location.origin}`,  },
     });
     if (error) console.error("Error with Google login:", error.message);
   };
